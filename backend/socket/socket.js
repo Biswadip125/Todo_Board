1e1158dab490490e66ae8cb2ff0e60edd3a0026a
Biswadip125 <162881134+Biswadip125@users.noreply.github.com>
2025-07-09 22:26:04 +0530

    Update socket.js for cors

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://todo-board-chi.vercel.app/",
+    origin: "https://todo-board-chi.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
