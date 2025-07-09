8f7949c7a143853e5480a620890a477b9649761a
Biswadip125 <162881134+Biswadip125@users.noreply.github.com>
2025-07-09 22:10:59 +0530

    Update app.js for cors

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://todo-board-chi.vercel.app/",
     credentials: true,
   })
 );
